47b0cc2e64d4d8ffa4c7a9e6aaf3410bb421d507
Matthias Katzfuss <37219335+katzfuss@users.noreply.github.com>
2023-03-15 09:57:44 -0500

    Update maternSine_comparison_all.R

--- code/maternSine_comparison_all.R
+++ code/maternSine_comparison_all.R
@@ -1,6 +1,5 @@
 #####  nonlinear sine map based on matern   #####
 
-# setwd("G:/My Drive/projects/triangular_spatial")
 source('code/nonlinearSpatial_functions.R')
 source('code/logScore_comparison.R')
 
